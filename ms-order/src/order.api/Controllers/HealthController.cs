4bf6d1146f1ac44013b4320353c3f9b05ece8507
Giovana <giirochas@hotmail.com>
2022-04-15 12:46:53 -0300

    adicionando projeto order

---
+++ ms-order/src/order.api/Controllers/HealthController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace order.api.Controllers
+{
+    public class HealthController : Controller
+    {
+        [Route("api/[controller]")]
+        public IActionResult Get()
+        {
+            return Ok(new { message = "App is running!" });
+        }
+    }
+}
