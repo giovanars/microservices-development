1b59b9d3eb031b7a592dd584fc0eca6ad2909c39
Giovana <giirochas@hotmail.com>
2022-04-26 11:27:43 -0300

    finalizando ms-menu

--- ms-menu/src/main/java/br/com/menu/MenuApplication.java
+++ ms-menu/src/main/java/br/com/menu/MenuApplication.java
@@ -1,9 +1,12 @@
 package br.com.menu;
 
+import br.com.menu.repositories.MenuRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories(basePackageClasses = MenuRepository.class)
 public class MenuApplication {
 
 	public static void main(String[] args) {
